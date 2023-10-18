038573122db63a3185fc908c317d84f2bce3b84b
Sergio Ricaflor <sergioricaflor@gmail.com>
2023-10-18 05:24:00 -0700

    websocket added to the server, added client and added a simple real time chat system

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+	console.log('a user has conected!')
+
+	socket.on('disconnect', () => {
+		console.log('an user has disconnected')
+	})
+
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg)
+	})
+})
+
 app.use(logger('dev')) 
 
 app.get('/', (req, res) => {
-	res.send('<h1>Esto es el chat</h1>')
+	res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Server running on port ${port}`)
 })
